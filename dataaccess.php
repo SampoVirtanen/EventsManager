9499cf59bee21b09405d2eb06d05d2f86d2bf5ca
SampoVirtanen <102716528+SampoVirtanen@users.noreply.github.com>
2023-08-30 17:10:33 +0300

    Tietomallin API

---
+++ dataaccess.php
@@ -0,0 +1,153 @@
+<?php
+include "datamodel.php";
+
+class DataAccess {
+    private $connection;
+    public function __construct($connection)
+    {
+        $this->connection = $connection;
+
+    }
+
+    public function addParticipant($participant) {
+        try {
+            $query = $this->connection->prepare(
+                "INSERT INTO participants (first_name, last_name, email) VALUES (:fn, :ln, :em)"
+            );
+            $query->execute(array(
+                ":fn" => $participant->getFirstName(),
+                ":ln" => $participant->getLastName(),
+                ":em" => $participant->getEmail()
+            ));
+        }
+        catch (PDOException $e) { 
+            die("VIRHE: " . $e->getMessage()); 
+        }
+    }
+
+    public function getParticipants() {
+        $participants = [];
+        try {
+            $query = $this->connection->prepare(
+                "SELECT * FROM participants"
+            );
+            $query->execute();
+        } 
+        catch (PDOException $e) {
+            die("VIRHE: " . $e->getMessage());
+        }
+        $result = $query->fetchAll();
+        foreach($result as $i){
+            $participant = new Participant($i[1], $i[2], $i[3]);
+            $participant->setID($i[0]);
+            array_push($participants, $participant);
+        }
+        return $participants;
+    }
+
+    public function updateParticipant($participant) {
+        try {
+            $query = $this->connection->prepare(
+                "UPDATE participants SET first_name = :fn, last_name = :ln, email = :em WHERE id=:id"
+            );
+            $query->execute(array(
+                ":fn" => $participant->getFirstName(),
+                ":ln" => $participant->getLastName(),
+                ":em" => $participant->getEmail(),
+                ":id" => $participant->getId(),
+            ));
+        }
+        catch (PDOException $e) { 
+            die("VIRHE: " . $e->getMessage()); 
+        }
+    }
+
+    public function deleteParticipant($participant) {
+        try {
+            $query = $this->connection->prepare(
+                "DELETE FROM participants WHERE id=:id"
+            );
+            $query->execute(array(
+                ":id" => $participant->getID()
+            ));
+        }
+        catch (PDOException $e) { 
+            die("VIRHE: " . $e->getMessage()); 
+        }
+    }
+
+    public function addEvent($event) {
+        try {
+            $query = $this->connection->prepare(
+                "INSERT INTO events (title, description, address, start_time, end_time) VALUES (:title, :desc, :address, :st, :et)"
+            );
+            $query->execute(array(
+                ":title" => $event->getTitle(),
+                ":desc" => $event->getDescription(),
+                ":address" => $event->getAddress(),
+                ":st" => $event->getStartTime()->format("Y-m-d H:i:s"),
+                ":et" => $event->getEndTime()->format("Y-m-d H:i:s")
+            ));
+        }
+        catch (PDOException $e) { 
+            die("VIRHE: " . $e->getMessage()); 
+        }
+    }
+
+    public function getEvents() {
+        $events = [];
+        try {
+            $query = $this->connection->prepare(
+                "SELECT * FROM events"
+            );
+            $query->execute();
+        } 
+        catch (PDOException $e) {
+            die("VIRHE: " . $e->getMessage());
+        }
+        $result = $query->fetchAll();
+        foreach($result as $i){
+            $event = new Event($i[1], $i[2], $i[3], $i[4], $i[5], []);
+            $event->setID($i[0]);
+            array_push($events, $event);
+        }
+        return $events;
+    }
+
+    public function updateEvent($event) {
+        try {
+            $query = $this->connection->prepare(
+                "UPDATE events SET title = :title, description = :desc, address = :address, start_time = :st, end_time = :et WHERE id=:id"
+            );
+            $query->execute(array(
+                ":title" => $event->getTitle(),
+                ":desc" => $event->getDescription(),
+                ":address" => $event->getAddress(),
+                ":st" => $event->getStartTime()->format("Y-m-d H:i:s"),
+                ":et" => $event->getEndTime()->format("Y-m-d H:i:s"),
+                ":id" => $event->getID()
+            ));
+        }
+        catch (PDOException $e) { 
+            die("VIRHE: " . $e->getMessage()); 
+        }
+
+    }
+    
+    public function deleteEvent($event) {
+        echo $event->getID();
+        try {
+            $query = $this->connection->prepare(
+                "DELETE FROM events WHERE id=:id"
+            );
+            $query->execute(array(
+                ":id" => $event->getID()
+            ));
+        }
+        catch (PDOException $e) { 
+            die("VIRHE: " . $e->getMessage()); 
+        }
+    }
+}
+
+?>
--- test.php
+++ test.php
@@ -1,37 +1,58 @@
 <?php
-include "datamodel.php";
+include "dataaccess.php";
+
+$servername = "localhost";
+$username = "";
+$password = "";
+try {
+       $connection = new PDO("mysql:host=$servername;dbname=events_manager", $username, $password);
+       $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+catch(PDOException $e)
+    {
+        echo "Ei yhteyttä tietokantaan!<br> " . $e->getMessage();
+    }
+
+$dataAccess = new DataAccess($connection);
+
 $participant1 = new Participant("Etunimi", "Sukunimi", "es@gmail.com");
 $participant2 = new Participant("Sampo", "Virtanen", "sv8693@edu.turku.fi");
 $participant3 = new Participant("Sdsafadgs", "Rtwtwwt", "sr@gmail.com");
 $participant4 = new Participant("Testi", "Cles", "tc@gmail.com");
 $participant5 = new Participant("Cvxcvxc", "Hdfhdfgdfg", "ch@gmail.com");
+$participant6 = new Participant("Delete", "test", "dt@gmail.com");
 
-$event1 = new Event("Ohjelmistokehittäjänä Toimiminen", "dhrhntynrtyrt", "Turku Amk", 1693371600, 1693396800, [$participant2, $participant4]);
-$event2 = new Event("dngdfngdfndghn", "fngdfngdfgnfdf", "dngdfndfnf", 1697009400, 1697374800, [$participant1, $participant2, $participant3, $participant4, $participant5]);
+$event1 = new Event("Ohjelmistokehittäjänä Toimiminen", "dhrhntynrtyrt", "Turku Amk", "2023-08-30 08:30:00", "2023-08-30 15:00:00", [$participant2, $participant4]);
+$event2 = new Event("dngdfngdfndghn", "fngdfngdfgnfdf", "dngdfndfnf", "2023-10-11 10:00:00", "2023-10-15 16:00:00", [$participant1, $participant2, $participant3, $participant4, $participant5]);
+$event3 = new Event("sgsdfgdfnbdyr", "dgdgndhnj", "yntyynhgn", "2024-05-23 18:45:00", "2024-05-25 10:26:00", [$participant1, $participant2, $participant3, $participant4, $participant5]);
+$event4 = new Event("Delete test", "Testaan toimiiko poisto", "sfdhtnbrenoenryo", "2023-08-30 17:02:00", "2023-08-30 17:03:00", [$participant1, $participant2, $participant3, $participant4, $participant5]);
 
-
-echo "<table>";
-echo "<tr>";
-echo "<th>Title</th>";
-echo "<th>Description</th>";
-echo "<th>Address</th>";
-echo "<th>Starttime</th>";
-echo "<th>Endtime</th>";
-echo "</tr>";
-echo "<tr>";
-echo "<td>", $event1->getTitle(), "</td>";
-echo "<td>", $event1->getDescription(), "</td>";
-echo "<td>", $event1->getAddress(), "</td>";
-echo "<td>", date_format($event1->getStartTime(), "d/m/Y H.i.s"), "</td>";
-echo "<td>", date_format($event1->getEndTime(), "d/m/Y H.i.s"), "</td>";
-echo "</tr>";
-echo "<tr>";
-echo "<td>", $event2->getTitle(), "</td>";
-echo "<td>", $event2->getDescription(), "</td>";
-echo "<td>", $event2->getAddress(), "</td>";
-echo "<td>", date_format($event2->getStartTime(), "d/m/Y H.i.s"), "</td>";
-echo "<td>", date_format($event2->getEndTime(), "d/m/Y H.i.s"), "</td>";
-echo "</tr>";
-echo "</table>";
-
+$dataAccess->addParticipant($participant1);
+$dataAccess->addParticipant($participant2);
+$dataAccess->addParticipant($participant3);
+$dataAccess->addParticipant($participant4);
+$dataAccess->addParticipant($participant5);
+$dataAccess->addParticipant($participant6);
+$participants = $dataAccess->getParticipants();
+echo '<pre>'; print_r($participants); echo '</pre>';
+$participant3New = $participants[2];
+$participant3New->setFirstName("Oetgfgdf");
+$participant3New->setLasttName("Kjlkhdffg");
+$participant3New->setEmail("ok@gmail.com");
+$dataAccess->updateParticipant($participant3New);
+$dataAccess->deleteParticipant($participants[5]);
+$dataAccess->addEvent($event1);
+$dataAccess->addEvent($event2);
+$dataAccess->addEvent($event3);
+$dataAccess->addEvent($event4);
+$events = $dataAccess->getEvents();
+echo '<pre>'; print_r($events); echo '</pre>';
+$event3New = $events[2];
+$event3New->setTitle("gfnfghn hjg");
+$event3New->setDescription("Weretwtrtwertwuertwertiuwer wret erterwi tre");
+$event3New->setAddress("Mnvdsnfvjdsbs");
+$event3New->setStartTime("2025-06-24 19:46:01");
+$event3New->setEndTime("2025-06-26 11:27:01");
+$dataAccess->updateEvent($event3New);
+$dataAccess->deleteEvent($events[3])
 ?>
